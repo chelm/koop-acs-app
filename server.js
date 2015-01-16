f16328e2c35aab6f8a5bb39a97e6e5c84a3d93e8
chelm <christopher.helm@gmail.com>
2015-01-16 14:06:09 -0600

    adding it all

---
+++ server.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+"use strict";
+
+var express = require("express"),
+  cors = require('cors'),
+  config = require("config"),
+  koop = require('koop')( config ),
+  acs = require('koop-acs'),
+
+
+//register providers with koop 
+koop.register( acs ); 
+
+// create an express app
+var app = express();
+app.use( cors() );
+
+// add koop middleware
+app.use( koop );
+
+app.get('/status', function(req, res){
+  res.json( koop.status );
+});
+
+app.set('view engine', 'ejs');
+
+// serve the index
+app.get("/", function(req, res, next) {
+  res.render(__dirname + '/views/index');
+});
+
+app.listen(process.env.PORT || config.server.port,  function() {
+  console.log("Listening at http://%s:%d/", this.address().address, this.address().port);
+});
+
